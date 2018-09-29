bea3f7e27c87d4f7c68a4e1f9d7fed0137952c5b
Flaque <econrad@zagmail.gonzaga.edu>
2018-09-29 11:02:43 -0700

    Remove r

--- main.go
+++ main.go
@@ -4,12 +4,9 @@ import (
 	"io"
 	"log"
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
 
 	http.HandleFunc("/health", healthHandler)
 	log.Fatal(http.ListenAndServe(":8080", nil))
