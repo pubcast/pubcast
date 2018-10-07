4bf25513ceeb5ae5b7e16a027e5768ab86626f8d
Flaque <econrad@zagmail.gonzaga.edu>
2018-10-06 19:26:34 -0700

    refactor activity into it's own structure

---
+++ lib/activity/vocab.go
@@ -0,0 +1,24 @@
+package activity
+
+import (
+	"net/url"
+)
+
+const context = "https://www.w3.org/ns/activitystreams"
+
+// An Organization actor
+// Ref: https://www.w3.org/TR/activitystreams-vocabulary/#dfn-organization
+type Organization struct {
+	Context string  `json:"@context"`
+	ID      url.URL `json:"id"`
+	Name    string  `json:"name"`
+}
+
+// NewOrganization creates an ActivityStreams Organization
+func NewOrganization(name string, id url.URL) Organization {
+	return Organization{
+		Context: context,
+		Name:    name,
+		ID:      id,
+	}
+}
